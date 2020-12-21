4472a2bceb94b4538756c476e058b85113de73f0
Enier Ramos García <enier290188@gmail.com>
2020-12-21 10:17:16 -0500

    2. Create a model/entity to store zip codes in the database.

---
+++ src/main/java/com/tts/WeatherApp/entity/ZipCode.java
@@ -0,0 +1,23 @@
+package com.tts.WeatherApp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ZipCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String zipCode;
+}
